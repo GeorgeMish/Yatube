c8199dab23da383abf930691cc94b05ec00e87e4
Yury Mishkevich <georgemishkevich@gmail.com>
2022-11-29 10:30:56 +0300

    mid 6th sprint

---
+++ yatube/posts/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from . import constants
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    """Создание модели Post."""
+
+    text = models.TextField(
+            'Текст поста',
+            help_text='Введите текст поста'
+    )
+    pub_date = models.DateTimeField(
+            'Дата публикации',
+            auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        related_name='posts')
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True,
+        verbose_name='Группа',
+        help_text='Группа, к которой будет относиться пост')
+    image = models.ImageField(
+        'Картинка',
+        upload_to='posts/',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        """Строковое представление объекта."""
+        return self.text[:constants.SYMBOLS_IN_SELF_TEXT]
+
+
+class Group(models.Model):
+    """Создание модели Group."""
+
+    title = models.CharField('Заголовок', max_length=200,
+                             help_text='Заголовок группы.')
+    slug = models.SlugField('URL', max_length=200, unique=True,
+                            help_text='Уникальный фрагмент адреса.')
+    description = models.TextField('Содержание')
+
+    def __str__(self):
+        """Возвращает название группы."""
+        return self.title
+
+
+class Comment(models.Model):
+    """Создание модели Comment."""
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments')
+
+    text = models.TextField('Текст комментария',
+                            help_text='Введите текст комментария'
+                            )
+    created = models.DateTimeField('Дата', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-created',)
+
+    def __str__(self):
+        """Строковое представление объекта."""
+        return self.text[:constants.SYMBOLS_IN_SELF_TEXT]
---
+++ yatube/posts/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
+from .models import Post, Group, User
+from .forms import PostForm, CommentForm
+from .utils import get_page_context
+
+
+def index(request):
+    """Выводит шаблон главной страницы."""
+    post_list = Post.objects.select_related('author', 'group')
+    page_obj = get_page_context(post_list, request)
+    context = {
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    """Выводит шаблон с постами группы."""
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.select_related('author').all()
+    page_obj = get_page_context(post_list, request)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    """Выводит страницу профиля пользователя."""
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.all()
+    page_obj = get_page_context(post_list, request)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    """Выводит страницу отдельно взятого поста."""
+    post_object = get_object_or_404(Post.objects.select_related(
+        'group', 'author'), id=post_id)
+    comments = post_object.comments.all()
+    count = post_object.author.posts.count()
+    form = CommentForm(request.POST or None)
+    context = {
+        'post': post_object,
+        'comments': comments,
+        'form': form,
+        'count': count
+    }
+
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    """Возможность создать новый пост для авторизованного пользователя."""
+    form = PostForm(request.POST or None,
+                    files=request.FILES or None)
+    if request.method == 'POST' and form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+
+        return redirect('posts:profile', request.user.username)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    """Возможность редактировать пост для авторизованного пользователя."""
+    post_object = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None,
+                    files=request.FILES or None,
+                    instance=post_object)
+    if post_object.author != request.user:
+        return redirect('posts:post_detail', post_id)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        if post_object.author == request.user:
+            return redirect('posts:post_edit', post_id)
+        form = PostForm(instance=post_object)
+
+    context = {
+        'form': form,
+        'True': True,
+        'post': post_object
+    }
+
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def add_comment(request, post_id):
+    """Возможность оставлять комментарии для авторизованного пользователя."""
+    post_object = get_object_or_404(Post, id=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post_object
+        comment.save()
+    return redirect('posts:post_detail', post_id=post_id)
